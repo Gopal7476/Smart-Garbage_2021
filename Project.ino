0c02e7120e5809ba4dfff829a1fad1f9aec752a3
Gopal7476 <82716272+Gopal7476@users.noreply.github.com>
2021-07-12 21:11:27 +0530

    Add files via upload

---
+++ Project.ino
@@ -0,0 +1,24 @@
+//Including libraries
+#include<ESP8266WiFi.h>  // Library for ESP8266 WiFi
+
+const char *ssid="Rajee";  //  local WiFi name  ssid(service set identifier)
+const char *pwd="Rajee@172730";  // local WiFi pwd(password)
+
+void setup() {
+  Serial.begin(9600);  //to STARTS serial communication
+  Serial.println("WiFi Connecting to "+String(ssid));  // WiFi Connection with local network
+  WiFi.begin(ssid,pwd);  // WiFi connection with SSID and Password
+  while(WiFi.status()!= WL_CONNECTED)  // WiFi Status Checking
+  {
+    Serial.print(".....");
+    delay(300);
+  }
+  Serial.println("\n WiFi Connected with "+String(ssid));  // WiFi Connected and display the IP address
+  Serial.println("IP Address : ");  // IP address of connected network
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
